72f36c2abdf6fbe024977aa9d41d75511d451acc
Mohamed Hayibor <mohamedhayibor@gmail.com>
2018-03-25 06:22:12 -0400

    Add web3 instance in db.cljs

--- src/cljs/ethervine/db.cljs
+++ src/cljs/ethervine/db.cljs
@@ -1,4 +1,8 @@
-(ns ethervine.db)
+(ns ethervine.db
+  (:require [cljs-web3.core :as web3]))
 
 (def default-db
-  {:name "re-frame"})
+  {:name "ethervine-db"
+   :web3 (or (aget js/window "web3")
+             ;; only on goog.DEBUG
+             (web3/create-web3 "http://localhost:8545"))})
